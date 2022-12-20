746b3c439f054d0a363c5acb9e28cfcd6df0c1db
Luca Ghislotti <ghislottiluca@gmail.com>
2022-12-20 18:40:39 +0100

    Added plots

--- scripts/main.py
+++ scripts/main.py
@@ -6,13 +6,18 @@ import matplotlib.pyplot as plt
 from read_events import *
 from read_pedestals import *
 
+# Filepaths for x-ray and pedestal
 filepath_xray_data = "xray_data\IT_400_xray_205_FTh_3mins_tau4.txt"
 filepath_pedestal_data = "pedestal_data\L4R0M0_Pedestals.dat"
+
+# Configuration data
 ch_min = 0
 ch_max = 31
-channels = range(ch_min, ch_max + 1)
 ASIC_number = 0
 pt = 4
+
+channels = range(ch_min, ch_max + 1)
+output_folder_path = r"C:\Users\ghisl\Documents\GitHub\GAPS_x-ray_calibration\output"
 
 # Events per channel organised in columns
 events = read_events(filepath_xray_data, ASIC_number)
@@ -21,25 +26,119 @@ events = read_events(filepath_xray_data, ASIC_number)
 pedestals = read_pedestals(filepath_pedestal_data)
 
 # Raw data histogram per channel
+raw_main_folder = os.path.join(output_folder_path, "raw_data")
+
+if not os.path.exists(raw_main_folder):
+    os.mkdir(raw_main_folder)
+
+raw_plot_folder = os.path.join(raw_main_folder, "plots")
+
+if not os.path.exists(raw_plot_folder):
+    os.mkdir(raw_plot_folder)
+
 print("\n***Saving raw data plots***")
 for ch in channels:
     plt.clf()
     binwidth = 1
     events_data = get_events(events, ch)
-    events_data = [dat_i - get_pedestal(pedestals, ch, pt) for dat_i in events_data]
     (n, bins, patches) = plt.hist(
         events_data,
-        bins=range(int(min(events_data)), int(max(events_data)) + binwidth, binwidth),
+        bins=range(
+            int(min(events_data)),
+            int(max(events_data)) + binwidth,
+            binwidth,
+        ),
+        color="teal",
     )
     plt.xlim(xmin=0, xmax=300)
     plt.yscale("log")
-    plt.title("Raw data for channel " + str(ch) + "at tau " + str(pt))
-    plt.savefig(
-        r"output\raw_data_no-pedestal\ch"
-        + str(ch)
-        + "_"
-        + "pt"
-        + str(pt)
-        + "_no-ped.pdf"
+    plt.title(
+        "Raw data for channel " + str(ch) + " at tau " + str(pt),
+        fontweight="bold",
     )
-    print("Saved ch. " + str(ch))
+    raw_data_plot = os.path.join(
+        raw_plot_folder, "ch" + str(ch) + "_" + "pt" + str(pt) + "_raw.pdf"
+    )
+    plt.savefig(raw_data_plot)
+
+    raw_data_folder = os.path.join(raw_main_folder, "data")
+
+    if not os.path.exists(raw_data_folder):
+        os.mkdir(raw_data_folder)
+
+    # Write raw data to file
+    raw_data_file = os.path.join(
+        raw_data_folder, "ch" + str(ch) + "_" + "pt" + str(pt) + "_raw.dat"
+    )
+    with open(
+        raw_data_file,
+        "w",
+    ) as fp:
+        for item in events_data:
+            fp.write(str(item) + "\n")
+
+    print("*Saved ch. " + str(ch) + "*")
+    print("plot: " + str(raw_data_plot))
+    print("data: " + str(raw_data_file) + "\n")
+
+# Raw data histogram per channel with pedestal subtracted
+raw_noped_main_folder = os.path.join(output_folder_path, "raw_no-pedestal_data")
+
+if not os.path.exists(raw_noped_main_folder):
+    os.mkdir(raw_noped_main_folder)
+
+raw_noped_plot_folder = os.path.join(raw_noped_main_folder, "plots")
+
+if not os.path.exists(raw_noped_plot_folder):
+    os.mkdir(raw_noped_plot_folder)
+
+print("\n***Saving raw data plots without pedestal***")
+for ch in channels:
+    plt.clf()
+    binwidth = 1
+    events_data = get_events(events, ch)
+    events_data_removed = [
+        dat_i - get_pedestal(pedestals, ch, pt) for dat_i in events_data
+    ]
+    (n, bins, patches) = plt.hist(
+        events_data_removed,
+        bins=range(
+            int(min(events_data_removed)),
+            int(max(events_data_removed)) + binwidth,
+            binwidth,
+        ),
+        color="firebrick",
+    )
+    plt.xlim(xmin=0, xmax=300)
+    plt.yscale("log")
+    plt.title(
+        "Raw data for channel " + str(ch) + " at tau " + str(pt) + " without pedestal",
+        fontweight="bold",
+    )
+
+    raw_noped_data_plot = os.path.join(
+        raw_noped_plot_folder,
+        "ch" + str(ch) + "_" + "pt" + str(pt) + "_raw_no-pedestal.pdf",
+    )
+    plt.savefig(raw_noped_data_plot)
+
+    raw_noped_data_folder = os.path.join(raw_noped_main_folder, "data")
+
+    if not os.path.exists(raw_noped_data_folder):
+        os.mkdir(raw_noped_data_folder)
+
+    # Write raw data to file
+    raw_noped_data_file = os.path.join(
+        raw_noped_data_folder,
+        "ch" + str(ch) + "_" + "pt" + str(pt) + "_raw_no-pedestal.dat",
+    )
+    with open(
+        raw_noped_data_file,
+        "w",
+    ) as fp:
+        for item in events_data:
+            fp.write(str(item) + "\n")
+
+    print("*Saved ch. " + str(ch) + "*")
+    print("plot: " + str(raw_noped_data_plot))
+    print("data: " + str(raw_noped_data_file) + "\n")
